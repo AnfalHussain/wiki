7aaaef52b6e98dfe551041ed1cb5e60bd3371634
AnfalHussain <anfalae95@gmail.com>
2019-08-21 15:35:45 +0300

    Anfal and Yousef H

--- pages/models.py
+++ pages/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from datatime import datatime
 
 class Page(models.Model):
 	title = models.CharField(max_length=120)
 	content = models.TextField()
-	last_update = models.DateTimeField(auto_now_add=True)
+	last_update = models.DateField(auto_now=True)
 
 	def get_absolute_url(self):
 		from django.urls import reverse
